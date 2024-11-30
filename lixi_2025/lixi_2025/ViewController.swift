1e64579bc2fd7c4b3c418e4c5a54df9f391675d6
iMan-sources <levietanh.ios.dev@gmail.com>
2024-11-30 10:17:39 +0700

    init

---
+++ lixi_2025/lixi_2025/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lixi_2025
+//
+//  Created by Le Viet Anh on 30/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
